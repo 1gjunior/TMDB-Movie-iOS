12684ce7a23c2e66c6d3b681e37bc50cbd143182
Gilberto Junior <gjunior@ciandt.com>
2022-07-12 12:56:02 -0300

    project setup configured podfile

---
+++ CI&T Movie Database/CI&T Movie Database/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CI&T Movie Database
+//
+//  Created by Gilberto Jose de Sousa Silva Junior on 12/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CI&T Movie Database/Pods/Target Support Files/Pods-CI&T Movie Database/Pods-CI&T Movie Database-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CI_T_Movie_Database : NSObject
+@end
+@implementation PodsDummy_Pods_CI_T_Movie_Database
+@end
