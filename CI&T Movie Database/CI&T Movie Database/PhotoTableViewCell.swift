7fe8f6df44a2154906ba9debe2c5bba4397505b7
Gilberto Junior <gjunior@ciandt.com>
2022-07-20 22:15:34 -0300

    stage 2 layout: add photos table view controller

---
+++ CI&T Movie Database/CI&T Movie Database/PhotoTableViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  PhotoTableViewCell.swift
+//  CI&T Movie Database
+//
+//  Created by Gilberto Junior on 20/07/22.
+//
+
+import UIKit
+
+class PhotoTableViewCell: UITableViewCell {
+    @IBOutlet var photoImageView: UIImageView!
+}
---
+++ CI&T Movie Database/CI&T Movie Database/PhotoViewController.swift
@@ -0,0 +1,46 @@
+//
+//  PhotoViewController.swift
+//  CI&T Movie Database
+//
+//  Created by Gilberto Junior on 20/07/22.
+//
+
+import UIKit
+
+class PhotoViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    @IBOutlet var table: UITableView!
+
+    struct MovieImage {
+        let title: String
+        let imageName: String
+    }
+
+    let data: [MovieImage] = [
+        MovieImage(title: "image 1", imageName: "image 1"),
+        MovieImage(title: "image 2", imageName: "image 2"),
+        MovieImage(title: "image 3", imageName: "image 3"),
+    ]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        table.dataSource = self
+        table.delegate = self
+    }
+
+    func tableView(_: UITableView, numberOfRowsInSection _: Int) -> Int {
+        return data.count
+    }
+
+    func tableView(_: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let photoMovie = data[indexPath.row]
+        let photoCell = table.dequeueReusableCell(withIdentifier: "photoCell", for: indexPath) as! PhotoTableViewCell
+
+        photoCell.photoImageView.image = UIImage(named: photoMovie.imageName)
+
+        return photoCell
+    }
+
+    func tableView(_: UITableView, heightForRowAt _: IndexPath) -> CGFloat {
+        return 224
+    }
+}
