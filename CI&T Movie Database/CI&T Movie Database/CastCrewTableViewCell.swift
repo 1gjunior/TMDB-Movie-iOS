500a6cb5445b6814e8fd93f3913439b3417ab6eb
Gilberto Junior <gjunior@ciandt.com>
2022-07-25 15:22:39 -0300

    stage 2 layout: remove unused comments and format the swift files of the project

--- CI&T Movie Database/CI&T Movie Database/CastCrewTableViewCell.swift
+++ CI&T Movie Database/CI&T Movie Database/CastCrewTableViewCell.swift
@@ -8,21 +8,8 @@
 import UIKit
 
 class CastCrewTableViewCell: UITableViewCell {
-
     @IBOutlet var profileImageView: UIImageView!
     @IBOutlet var realName: UILabel!
     @IBOutlet var threeDotsImageView: UIImageView!
     @IBOutlet var charName: UILabel!
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
 }
--- CI&T Movie Database/CI&T Movie Database/CastCrewTableViewController.swift
+++ CI&T Movie Database/CI&T Movie Database/CastCrewTableViewController.swift
@@ -8,17 +8,16 @@
 import UIKit
 
 class CastCrewTableViewController: UITableViewController {
-
     @IBOutlet var table: UITableView!
-    
+
     struct Cast {
         let profileImage: String
         let realName: String
         let threeDots: String
         let charName: String
     }
-    
-     let data: [Cast] = [
+
+    let data: [Cast] = [
         Cast(profileImage: "Keanu_Reeves", realName: "Keanu Reeves", threeDots: "Three_Dots", charName: "John Wick"),
         Cast(profileImage: "Halle_Berry", realName: "Halle Berry", threeDots: "Three_Dots", charName: "Sofia"),
         Cast(profileImage: "Laurence_Fishburne", realName: "Laurence Fishburne", threeDots: "Three_Dots", charName: "Bowery King"),
@@ -29,34 +28,21 @@ class CastCrewTableViewController: UITableViewController {
         Cast(profileImage: "Margaret_Daly", realName: "Margaret Daly", threeDots: "Three_Dots", charName: "Operator"),
         Cast(profileImage: "Jerome_Flynn", realName: "Jerome Flynn", threeDots: "Three_Dots", charName: "Berrada"),
     ]
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         table.dataSource = self
         table.delegate = self
-        
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
-    // MARK: - Table view data source
-
-//    override func numberOfSections(in tableView: UITableView) -> Int {
-//        // #warning Incomplete implementation, return the number of sections
-//        return 0
-//    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    override func tableView(_: UITableView, numberOfRowsInSection _: Int) -> Int {
         return data.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cast = data[indexPath.row]
         let castCell = tableView.dequeueReusableCell(withIdentifier: "castCell", for: indexPath) as! CastCrewTableViewCell
-        
+
         castCell.profileImageView.image = UIImage(named: cast.profileImage)
         castCell.realName.text = cast.realName
         castCell.threeDotsImageView.image = UIImage(named: cast.threeDots)
@@ -64,50 +50,4 @@ class CastCrewTableViewController: UITableViewController {
 
         return castCell
     }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
