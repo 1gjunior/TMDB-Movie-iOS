aab3335d630a38303ee2bd94838f8827cf15f802
Gilberto Junior <gjunior@ciandt.com>
2022-08-09 22:27:32 -0300

    stage 4 architecture: add api manager service

---
+++ CI&T Movie Database/CI&T Movie Database/Services/APIManagerService.swift
@@ -0,0 +1,29 @@
+//
+//  APIManagerService.swift
+//  CI&T Movie Database
+//
+//  Created by Gilberto Junior on 09/08/22.
+//
+
+import Combine
+import Foundation
+
+class APIManagerService {
+    
+    public let apiBaseURL = "https://api.themoviedb.org/3/movie"
+    public let apiKey = "a5a29cab08554d8a0b331b250a19170b"
+    
+    private var subscribers = Set<AnyCancellable>()
+
+    func fetchItems<T: Decodable>(url: URL, completion: @escaping (Result<T, Error>) -> Void) {
+        URLSession.shared.dataTaskPublisher(for: url).map { $0.data }.decode(type: T.self, decoder: JSONDecoder()).sink(receiveCompletion: { resultCompletion in
+            switch resultCompletion {
+            case .failure(let error):
+                completion(.failure(error))
+            case .finished: break
+            }
+        }, receiveValue: { resultArray in
+            completion(.success(resultArray))
+        }).store(in: &subscribers)
+    }
+}
